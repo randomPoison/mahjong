08165b468e8849eb8b345b5f3904321c436ea27a
David LeGare <dlegare.1001@gmail.com>
2019-12-13 16:02:41 -0600

    Fix JSON serialization tests

--- Mahjong.Tests/Mahjong_TileSerializationShould.cs
+++ Mahjong.Tests/Mahjong_TileSerializationShould.cs
@@ -1,23 +1,35 @@
+using System;
+using System.IO;
 using DotNetGame.Mahjong;
 using Newtonsoft.Json;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Mahjong.Tests
 {
     public class Mahjong_TileSerializationShould
     {
+        private readonly ITestOutputHelper _output;
+
+        public Mahjong_TileSerializationShould(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         [Fact]
         public void SimpleTile_SerializeJson()
         {
             var tile = new SimpleTile(Suit.Coins, 1);
-            var json = JsonConvert.ToString(tile);
+
+            var json = JsonConvert.SerializeObject(tile);
+            _output.WriteLine(json);
         }
 
         [Fact]
         public void SimpleTile_AsITile_SerializeJson()
         {
             ITile tile = new SimpleTile(Suit.Coins, 1);
-            var json = JsonConvert.ToString(tile);
+            var json = JsonConvert.SerializeObject(tile);
         }
     }
 }
