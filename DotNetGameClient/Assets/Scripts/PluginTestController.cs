26063e77d74f0598cebd0e4bef6d2ba4486ca10a
David LeGare <dlegare.1001@gmail.com>
2020-01-10 16:01:11 -0600

    Setup plugin test script

---
+++ DotNetGameClient/Assets/Scripts/PluginTestController.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Synapse.Mahjong
+{
+    public class PluginTestController : MonoBehaviour
+    {
+        // TODO: Move the plugin interop into its own class.
+        [DllImport("__Internal", EntryPoint = "generate_tileset")]
+        public static extern string GenerateTileset();
+
+        private void Start()
+        {
+            var tilesetJson = GenerateTileset();
+            Debug.Log($"Tileset JSON: {tilesetJson}");
+        }
+    }
+}
