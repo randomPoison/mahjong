7245f12cec99adbe59dd09b640621486ab9643a6
David LeGare <dlegare.1001@gmail.com>
2020-01-17 17:06:30 -0600

    Call into a Rust DLL from C#

--- DotNetGameClient/Assets/Scripts/PluginTestController.cs
+++ DotNetGameClient/Assets/Scripts/PluginTestController.cs
@@ -4,9 +4,12 @@ namespace Synapse.Mahjong
 {
     public class PluginTestController : MonoBehaviour
     {
+        [SerializeField]
+        private Mahjong _plugin;
+
         private void Start()
         {
-            var tilesetJson = Mahjong.GenerateTileset();
+            var tilesetJson = _plugin.GenerateTileset();
             Debug.Log($"Tileset JSON: {tilesetJson}");
         }
     }
--- DotNetGameClient/Packages/com.synapse-games.mahjong/Mahjong.cs
+++ DotNetGameClient/Packages/com.synapse-games.mahjong/Mahjong.cs
@@ -1,10 +1,38 @@
+using System;
 using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
 
 namespace Synapse.Mahjong
 {
-    public class Mahjong
+    public class Mahjong : MonoBehaviour
     {
-        [DllImport("__Internal")]
-        public static extern string GenerateTileset();
+        [DllImport(
+            "mahjong",
+            EntryPoint = "__cs_bindgen_generate_tileset_json",
+            CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr GenerateTilesetJson(out long length);
+
+        [DllImport(
+            "mahjong",
+            EntryPoint = "__cs_bindgen_drop_string",
+            CallingConvention = CallingConvention.Cdecl)]
+        private static extern void DropString(IntPtr raw);
+
+        public string GenerateTileset()
+        {
+            var rawPtr = GenerateTilesetJson(out var length);
+            Debug.Log($"Raw pointer: {rawPtr}, length: {length}");
+
+            string result;
+            unsafe
+            {
+                result = Encoding.UTF8.GetString((byte*)rawPtr, (int)length);
+            }
+
+            DropString(rawPtr);
+
+            return result;
+        }
     }
 }
