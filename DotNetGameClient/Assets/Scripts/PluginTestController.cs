ca17f9b658afba4bf604e771da0b6156f2a48ec0
David LeGare <dlegare.1001@gmail.com>
2020-01-24 16:21:35 -0600

    Replace manual bindings with machine-generated ones

--- DotNetGameClient/Assets/Scripts/PluginTestController.cs
+++ DotNetGameClient/Assets/Scripts/PluginTestController.cs
@@ -4,13 +4,9 @@ namespace Synapse.Mahjong
 {
     public class PluginTestController : MonoBehaviour
     {
-        [SerializeField]
-        private Mahjong _plugin;
-
         private void Start()
         {
-            var tilesetJson = _plugin.GenerateTileset();
-            Debug.Log($"Tileset JSON: {tilesetJson}");
+            var tilesetJson = global::Mahjong.GenerateTilesetJson();
         }
     }
 }
--- DotNetGameClient/Packages/com.synapse-games.mahjong/Mahjong.cs
+++ DotNetGameClient/Packages/com.synapse-games.mahjong/Mahjong.cs
@@ -1,37 +1,27 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Text;
-using UnityEngine;
 
-namespace Synapse.Mahjong
+public class Mahjong
 {
-    public class Mahjong : MonoBehaviour
+    [DllImport("mahjong", EntryPoint = "__cs_bindgen_generated_generate_tileset_json", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr __GenerateTilesetJson(out int length);
+
+    [DllImport("mahjong", EntryPoint = "__cs_bindgen_drop_string", CallingConvention = CallingConvention.Cdecl)]
+    private static extern void DropString(IntPtr raw);
+
+    public static string GenerateTilesetJson()
     {
-        [DllImport(
-            "mahjong",
-            EntryPoint = "__cs_bindgen_generated_generate_tileset_json",
-            CallingConvention = CallingConvention.Cdecl)]
-        private static extern IntPtr GenerateTilesetJson(out int length);
+        var rawResult = __GenerateTilesetJson(out var length);
 
-        [DllImport(
-            "mahjong",
-            EntryPoint = "__cs_bindgen_drop_string",
-            CallingConvention = CallingConvention.Cdecl)]
-        private static extern void DropString(IntPtr raw);
-
-        public string GenerateTileset()
+        string result;
+        unsafe
         {
-            var rawPtr = GenerateTilesetJson(out var length);
-
-            string result;
-            unsafe
-            {
-                result = Encoding.UTF8.GetString((byte*)rawPtr, length);
-            }
-
-            DropString(rawPtr);
-
-            return result;
+            result = Encoding.UTF8.GetString((byte*)rawResult, length);
         }
+
+        DropString(rawResult);
+
+        return result;
     }
 }
