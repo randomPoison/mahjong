c9af48ead7198d558b3bfd988254a0b771764028
David LeGare <dlegare.1001@gmail.com>
2020-02-03 20:25:06 -0600

    Add handle_handshake_response function (not yet working)

--- mahjong-shared/src/lib.rs
+++ mahjong-shared/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod messages;
 pub mod tile;
 
-use crate::messages::{HandshakeRequest, Version};
+use crate::messages::{HandshakeRequest, HandshakeResponse, Version};
 use cs_bindgen::prelude::*;
 
 cs_bindgen::generate_static_bindings!();
@@ -16,4 +16,13 @@ pub fn create_handshake_request() -> String {
     };
 
     serde_json::to_string(&request).expect("Failed to serialize `HandshakeRequest`")
+}
+
+#[cs_bindgen]
+pub fn handle_handshake_response(json: String) -> u64 {
+    let message = serde_json::from_str::<HandshakeResponse>(&json)
+        .expect("Failed to deserialize `HandshakeResponse`");
+
+    // Return the player's point total.
+    message.account_data.points
 }
