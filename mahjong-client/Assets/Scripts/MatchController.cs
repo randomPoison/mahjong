93f77d4bd4aee48124e16a5c51e9e5c6ea0a12a6
David LeGare <dlegare.1001@gmail.com>
2020-04-18 22:38:29 -0500

    Instantiate tiles at the start of a match

--- mahjong-client/Assets/Scripts/MatchController.cs
+++ mahjong-client/Assets/Scripts/MatchController.cs
@@ -1,4 +1,5 @@
-﻿using Synapse.Utils;
+﻿using System;
+using Synapse.Utils;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
@@ -9,6 +10,14 @@ namespace Synapse.Mahjong
     /// </summary>
     public class MatchController : MonoBehaviour
     {
+        // TODO: Figure out a better way to track tile dimensions. This should likely be
+        // tracked along with the tile asset set, once we move the tile set to a custom
+        // asset.
+        private const float TileWidth = 0.026f;
+
+        private const float LeftSide = TileWidth * TilesInAHand * -0.5f;
+        private const int TilesInAHand = 13;
+
         [SerializeField]
         [Tooltip(
             "The root object for each player's hand. Tiles in each players' hand will " +
@@ -45,27 +54,32 @@ namespace Synapse.Mahjong
             _state = _client.HandleStartMatchResponse(responseJson);
             Debug.Log($"Started match, ID: {_state.Id()}", this);
 
+            // Once we have the match data, instantiate the tiles for each player's
+            // starting hand.
+
             foreach (var seat in EnumUtils.GetValues<Wind>())
             {
+                var handRoot = _handRoots[(int)seat];
                 var tiles = _state.GetPlayerHand(seat);
-                Debug.Log($"Tiles @ {seat}: {tiles.Count}");
 
                 foreach (var (index, tile) in tiles.Enumerate())
                 {
-                    switch (tile)
-                    {
-                        case Tile.Simple simple:
-                            Debug.Log($"Tile @ {seat} #{index}: {simple.Element0.Number} of {simple.Element0.Suit}");
-                            break;
+                    // Instantiate the prefab for the tile.
+                    //
+                    // TODO: Load the tile assets ahead of time. This setup is going to
+                    // be unnecessarily slow because we load the tiles one at a time. We
+                    // should preload all tiles from the set at startup, since we know
+                    // we're going to need them all.
+                    var asset = GetTileAsset(tile);
+                    var tileObject = await Addressables.InstantiateAsync(asset).Task;
 
-                        case Tile.Dragon dragon:
-                            Debug.Log($"Tile @ {seat} #{index}: {dragon.Element0} Dragon");
-                            break;
-
-                        case Tile.Wind wind:
-                            Debug.Log($"Tile @ {seat} #{index}: {wind.Element0} Wind");
-                            break;
-                    }
+                    // Make the tile a child of the root object for the player's hand,
+                    // and position it horizontally.
+                    tileObject.transform.SetParent(handRoot, false);
+                    tileObject.transform.localPosition = new Vector3(
+                        LeftSide + TileWidth * index,
+                        0f,
+                        0f);
                 }
             }
         }
@@ -74,6 +88,35 @@ namespace Synapse.Mahjong
         {
             _state?.Dispose();
             _state = null;
+        }
+
+        private AssetReference GetTileAsset(ITile tile)
+        {
+            switch (tile)
+            {
+                case Tile.Simple simple:
+                    // Tile numbers start at 1, so we need to subtract 1 to get the
+                    // index corresponding to the tile's numeric value.
+                    int tileIndex = simple.Element0.Number - 1;
+
+                    switch (simple.Element0.Suit)
+                    {
+                        case Suit.Bamboo: return _bambooTiles[tileIndex];
+                        case Suit.Characters: return _characterTiles[tileIndex];
+                        case Suit.Coins: return _circleTiles[tileIndex];
+
+                        default: throw new ArgumentException(
+                            $"Invalid suit {simple.Element0.Suit}");
+                    }
+
+                case Tile.Dragon dragon:
+                    return _dragonTiles[(int)dragon.Element0];
+
+                case Tile.Wind wind:
+                    return _windTiles[(int)wind.Element0];
+
+                default: throw new ArgumentException($"Invalid tile kind {tile}");
+            }
         }
     }
 }
