90dd2781ce8974a8cf6a3e350654e7e6ef4f3d18
David LeGare <dlegare.1001@gmail.com>
2020-04-20 09:00:08 -0500

    Document WithCancellation

--- mahjong-client/Assets/Scripts/TaskUtils.cs
+++ mahjong-client/Assets/Scripts/TaskUtils.cs
@@ -4,12 +4,27 @@ using System.Threading.Tasks;
 
 public static class TaskUtils
 {
+    /// <summary>
+    /// Adds a cancellation token to a task after its creation.
+    /// </summary>
+    ///
+    /// <param name="task">The task to append the cancellation token to.</param>
+    /// <param name="cancellationToken">The cancellation token to append.</param>
+    ///
+    /// <returns>
+    /// A new task that will either resolve to the value resolved by <paramref name="task"/>,
+    /// or will be canceled when <paramref name="cancellationToken"/> fires.
+    /// </returns>
     public static async Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken)
     {
         var tcs = new TaskCompletionSource<bool>();
         using (cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).TrySetResult(true), tcs))
+        {
             if (task != await Task.WhenAny(task, tcs.Task))
+            {
                 throw new OperationCanceledException(cancellationToken);
+            }
+        }
         return await task;
     }
 }
